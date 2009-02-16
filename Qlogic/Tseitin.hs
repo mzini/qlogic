3d1686779b457b1dca215559cea49651a0725921
andreas.schnabl <andreas.schnabl@uibk.ac.at>
2009-02-16 16:31:56 +0100

    20090216e
    mehr Tseitin
    
    darcs-hash:20090216153156-c6f3f-86ceba9cde27b989537de36faca8b7bdf6e1a376.gz

--- Qlogic/Tseitin.hs
+++ Qlogic/Tseitin.hs
@@ -34,10 +34,11 @@ lit Bot = BotLit
 lit (Neg x) = nlit x
 lit fm      = PosLit $ L fm
 
-nlit fm = negate' $ lit fm
-  where negate' (PosLit x) = NegLit x
-        negate' (NegLit x) = PosLit x
-        negate' _ = error "Cannot happen"
+nlit fm = negate $ lit fm
+  where negate (PosLit x) = NegLit x
+        negate (NegLit x) = PosLit x
+        negate TopLit = BotLit
+        negate BotLit = TopLit
 
 -- | The state monad for constructing CNFs exploits sharing by keeping
 -- a record of so far translated subformulas
@@ -104,26 +105,25 @@ transformPlus fm@(a `Imp` b) =
   maybeComputePos fm $
   do cnfA <- transformMinus a
      cnfB <- transformPlus b
-     return $ [[nlit fm, nlit a, lit b]] +&+ cnfA +&+ cnfB -- MA: verify - AS: sieht gut aus
+     return $ [[nlit fm, nlit a, lit b]] +&+ cnfA +&+ cnfB
      -- bigAnd [lvar fm `Imp` (lvar a `Imp` lvar b), cnfA, cnfB]
 transformPlus fm@(Neg a)       = maybeComputePos fm $ transformMinus a
-transformPlus fm@(Var _)       = maybeComputePos fm $ return [] -- MA: verify LITERALE EIGENS BEHANDELN!
-transformPlus Top              = maybeComputePos Top $ return [] -- MA: verify
-transformPlus Bot              = maybeComputePos Bot $ return [] -- MA: verify - AS: return TRUE fuer literale, die
-                                                                 -- "falsitaet" von Bottom wir weiter oben behandelt
+transformPlus fm@(Var _)       = maybeComputePos fm $ return []
+transformPlus Top              = maybeComputePos Top $ return []
+transformPlus Bot              = maybeComputePos Bot $ return []
 
 transformMinus fm@(a `And` b) =
   maybeComputeNeg fm $
   do cnfA <- transformMinus a
      cnfB <- transformMinus b
      return $ [[nlit a, nlit b, lit fm]] +&+ cnfA +&+ cnfB
-            -- bigAnd [(lvar a `And` lvar b) `Imp` (lvar fm), cnfA, cnfB] -- MA: korrigiert
+            -- bigAnd [(lvar a `And` lvar b) `Imp` (lvar fm), cnfA, cnfB]
 transformMinus fm@(a `Or` b) =
   maybeComputeNeg fm $ 
   do cnfA <- transformMinus a
      cnfB <- transformMinus b
      return $ [[nlit a, lit fm], [nlit b, lit fm]] +&+ cnfA +&+ cnfB
-     -- [(lvar a `Or` lvar b) `Imp` (lvar fm)  , cnfA, cnfB] -- MA: korrigiert
+     -- [(lvar a `Or` lvar b) `Imp` (lvar fm)  , cnfA, cnfB]
      -- -a & -b | fm === (-a | fm) & (-b | fm)
 transformMinus fm@(a `Iff` b) =
   maybeComputeNeg fm $
@@ -132,26 +132,23 @@ transformMinus fm@(a `Iff` b) =
      cnfBpos <- transformPlus b
      cnfBneg <- transformMinus b
      return $ [[lit fm, lit a, lit b], [lit fm, nlit a, nlit b]] +&+ cnfApos +&+ cnfBpos +&+ cnfAneg +&+ cnfBneg
-        -- MA: verify - AS: kleine Korrektur
 
 transformMinus fm@(a `Imp` b) =
   maybeComputeNeg fm $
-  do cnfA <- transformMinus a
-     cnfB <- transformPlus b
-     return $ [[nlit fm, nlit a, lit b]] +&+ cnfA +&+ cnfB
--- bigAnd [lvar fm `Imp` (lvar a `Imp` lvar b), cnfA, cnfB] MA:TODO: verify
+  do cnfA <- transformPlus a
+     cnfB <- transformMinus b
+     return $ [[lit fm, lit a], [lit fm, nlit b]] +&+ cnfA +&+ cnfB
+-- bigAnd [lvar (lvar a `Imp` lvar b) `Imp` fm, cnfA, cnfB]
 
-transformMinus fm@(Neg a)     = maybeComputeNeg fm $ transformPlus a   -- MA:TODO: war transformMinus
-transformMinus fm@(Var _)     = maybeComputeNeg fm $ return $ [] --TODO:MA: verify
-transformMinus Top            = maybeComputeNeg Top $ return [] -- TODO:MA: verify
-transformMinus Bot            = maybeComputeNeg Bot $ return [[]] -- TODO:MA: verify
+transformMinus fm@(Neg a)     = maybeComputeNeg fm $ transformPlus a
+transformMinus fm@(Var _)     = maybeComputeNeg fm $ return $ []
+transformMinus Top            = maybeComputeNeg Top $ return []
+transformMinus Bot            = maybeComputeNeg Bot $ return []
 
 transformList :: Ord a => [Formula a] -> PGSetMonad a (CNF (ExtendedAtom a))
 transformList fms = (mapM transform_ fms)  >>= return . concat
-  where transform_ (Neg fm) = do cnf <- transformMinus fm
-                                 return $ [[nlit fm]] +&+ cnf
-        transform_ fm       = do cnf <- transformMinus fm
-                                 return $ [[lit fm]] +&+ cnf
+  where transform_ fm = do cnf <- transformPlus fm
+                           return $ [[lit fm]] +&+ cnf
 
 transform :: Ord a => Formula a -> CNF (ExtendedAtom a)
 transform fm = State.evalState (transformList $ splitAnd fm) $ St {posSet = Set.empty, negSet = Set.empty}
@@ -166,34 +163,6 @@ simplify (Neg  (Neg a))   = a
 simplify (Neg  ((Neg a) `And` (Neg b)))   = a `Or` b
 simplify (Neg  ((Neg a) `Or` (Neg b)))   = a `And` b
 simplify _ = undefined  -- TODO finish
-
--- toCNF :: Formula a -> Maybe (CNF a)
--- toCNF (x `And` y) = do xres <- toCNF x
---                        yres <- toCNF y
---                        return $ xres ++ yres
-
--- toCNF Top = Just []
--- toCNF (Neg Bot) = Just []
-
--- toCNF (x `Iff` y) = toCNF $ (neg x ||| y) &&& (x ||| neg y)
-
--- toCNF f = do xres <- toClause f
---              return [xres]
-
--- toClause :: Formula a -> Maybe (Clause a)
--- toClause (_ `And` _)   = Nothing
--- toClause (x `Or` y)    = do cx <- toClause x
---                             cy <- toClause y
---                             return $ cx ++ cy
--- toClause (Neg (Var x)) = Just [NegLit x]
--- toClause (Neg Top)     = Just []
--- toClause (Neg Bot)     = Just [TopLit]
--- toClause (Neg _)       = Nothing
--- toClause (Var x)       = Just [PosLit x]
--- toClause (x `Imp` y)   = toClause $ neg x ||| y
--- toClause Bot           = Just []
--- toClause Top           = Just [TopLit]
--- toClause (_ `Iff` _)   = Nothing
 
 baseAssignment :: Ord a => Assign (ExtendedAtom a) -> Assign a
 baseAssignment = fromMap . Map.foldWithKey f empty . toMap
