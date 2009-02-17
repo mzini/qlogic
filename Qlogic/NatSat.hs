1d3516f8d090a518cde54e9bf3875376ad47379f
andreas.schnabl <andreas.schnabl@uibk.ac.at>
2009-02-17 17:36:16 +0100

    20090217d
    Transformation Dio -> Nat
    
    darcs-hash:20090217163616-c6f3f-6590bcd09c1fa6be4d050ed6c3a8eeb8fd4b0aa5.gz

--- Qlogic/NatSat.hs
+++ Qlogic/NatSat.hs
@@ -3,7 +3,7 @@ module Qlogic.NatSat where
 import Qlogic.Formula
 
 type NatFormula a = [Formula a]
-type PLVec a = (a, Int)
+data PLVec a = PLVec a Int
 
 natToFormula :: Int -> NatFormula a
 natToFormula n | n == 0    = [Bot]
@@ -22,39 +22,49 @@ truncBots f@[n]    = f
 truncBots (Bot:ps) = truncBots ps
 truncBots f@(_:_)  = f
 
-plus :: NatFormula a -> NatFormula a -> NatFormula a
-plus [] []                  = []
-plus [p] [q]                = [p &&& q, neg (p <-> q)]
-plus ps qs | lengthdiff > 0 = plus (padBots lengthdiff ps) qs
-           | lengthdiff < 0 = plus ps $ padBots (-1 * lengthdiff) qs
-           | otherwise      = twoOrThree p q r : oneOrThree p q r : tail rs
+(.+.) :: NatFormula a -> NatFormula a -> NatFormula a
+[] .+. []                  = []
+[p] .+. [q]                = [p &&& q, neg (p <-> q)]
+ps .+. qs | lengthdiff > 0 = padBots lengthdiff ps .+. qs
+          | lengthdiff < 0 = ps .+. padBots (-1 * lengthdiff) qs
+          | otherwise      = twoOrThree p q r : oneOrThree p q r : tail rs
   where lengthdiff = length qs - length ps
-        rs         = plus (tail ps) (tail qs)
+        rs         = tail ps .+. tail qs
         p          = head ps
         q          = head qs
         r          = head rs
 
-times :: NatFormula a -> NatFormula a -> NatFormula a
-times ps []     = []
-times ps [q]    = map (&&& q) ps
-times ps (q:qs) = plus r1 r2
+bigPlus :: [NatFormula a] -> NatFormula a
+bigPlus = foldr (.+.) [Bot]
+
+(.*.) :: NatFormula a -> NatFormula a -> NatFormula a
+ps .*. []     = []
+ps .*. [q]    = map (&&& q) ps
+ps .*. (q:qs) = r1 .+. r2
   where r1 = (map (&&& q) ps) ++ padBots (length qs) []
-        r2 = times ps qs
+        r2 = ps .*. qs
 
-gt :: NatFormula a -> NatFormula a -> Formula a
-gt [] []                  = Bot
-gt [p] [q]                = p &&& neg q
-gt ps qs | lengthdiff > 0 = gt (padBots lengthdiff ps) qs
-         | lengthdiff < 0 = gt ps (padBots (-1 * lengthdiff) qs)
-         | otherwise      = (p &&& neg q) ||| ((p <-> q) &&& gt (tail ps) (tail qs))
-  where lengthdiff        = length qs - length ps
-        p                 = head ps
-        q                 = head qs
+bigTimes :: [NatFormula a] -> NatFormula a
+bigTimes = foldr (.*.) [Top]
 
-eq :: NatFormula a -> NatFormula a -> Formula a
-eq [] []                  = Top
-eq [p] [q]                = p <-> q
-eq ps qs | lengthdiff > 0 = eq (padBots lengthdiff ps) qs
-         | lengthdiff < 0 = eq ps (padBots (-1 * lengthdiff) qs)
-         | otherwise      = ((head ps) <-> (head qs)) &&& eq (tail ps) (tail qs)
-  where lengthdiff        = length qs - length ps
+(.>.) :: NatFormula a -> NatFormula a -> Formula a
+[] .>. []                  = Bot
+[p] .>. [q]                = p &&& neg q
+ps .>. qs | lengthdiff > 0 = padBots lengthdiff ps .>. qs
+          | lengthdiff < 0 = ps .>. padBots (-1 * lengthdiff) qs
+          | otherwise      = (p &&& neg q) ||| ((p <-> q) &&& (tail ps .>. tail qs))
+   where lengthdiff        = length qs - length ps
+         p                 = head ps
+         q                 = head qs
+
+(.=.) :: NatFormula a -> NatFormula a -> Formula a
+[] .=. []                  = Top
+[p] .=. [q]                = p <-> q
+ps .=. qs | lengthdiff > 0 = padBots lengthdiff ps .=. qs
+          | lengthdiff < 0 = ps .=. padBots (-1 * lengthdiff) qs
+          | otherwise      = ((head ps) <-> (head qs)) &&& (tail ps .=. tail qs)
+   where lengthdiff        = length qs - length ps
+
+varToNat :: Int -> a -> NatFormula (PLVec a)
+varToNat n v | n > 0     = varToNat (n - 1) v ++ [Var (PLVec v n)]
+             | otherwise = []
