b2c4e46347e7eecc04121fcb59e4ad2650518efe
andreas.schnabl <andreas.schnabl@uibk.ac.at>
2009-02-17 12:17:12 +0100

    20090217b
    NatSat.hs
    
    darcs-hash:20090217111712-c6f3f-62002dd53e5807b003828b96ae3933823ea13d8a.gz

---
+++ Qlogic/NatSat.hs
@@ -0,0 +1,42 @@
+module Qlogic.NatSat where
+
+import Qlogic.Formula
+
+type NatFormula a = [Formula a]
+
+natToFormula :: Int -> NatFormula a
+natToFormula n | n == 0    = [Bot]
+               | n == 1    = [Top]
+               | n < 0     = error "Only natural numbers allowed in argument!"
+               | otherwise = natToFormula (n `div` 2) ++ natToFormula (n `mod` 2)
+
+padBots :: Int -> NatFormula a -> NatFormula a
+padBots n | n == 0    = id
+          | n > 0     = (:) Bot . padBots (n - 1)
+          | otherwise = error "Only natural numbers allowed in argument!"
+
+truncBots :: NatFormula a -> NatFormula a
+truncBots []       = []
+truncBots f@[n]    = f
+truncBots (Bot:ps) = truncBots ps
+truncBots f@(_:_)  = f
+
+plus :: NatFormula a -> NatFormula a -> NatFormula a
+plus [] []                  = []
+plus [p] [q]                = [p &&& q, neg (p <-> q)]
+plus ps qs | lengthdiff > 0 = plus (padBots lengthdiff ps) qs
+           | lengthdiff < 0 = plus ps $ padBots (-1 * lengthdiff) qs
+           | otherwise      = twoOrThree p q r : oneOrThree p q r : tail rs
+  where plength    = length ps
+        lengthdiff = length qs - plength
+        rs         = plus (tail ps) (tail qs)
+        p          = head ps
+        q          = head qs
+        r          = head rs
+
+times :: NatFormula a -> NatFormula a -> NatFormula a
+times ps []     = []
+times ps [q]    = map (&&& q) ps
+times ps (q:qs) = plus r1 r2
+  where r1 = (map (&&& q) ps) ++ padBots (length qs) []
+        r2 = times ps qs
