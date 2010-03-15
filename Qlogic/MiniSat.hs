8878269e5d4dc60a67740586deca1845524511ee
martin.avanzini <martin.avanzini@uibk.ac.at>
2010-03-15 15:43:20 +0100

    replaced forkIO by spawn
    Ignore-this: 8c1ef71be16fa83065229ebf66cad442
    
    darcs-hash:20100315144320-3c1c7-679f0910a04c3d76c390ede56d4a86f47a7ed714.gz

--- Qlogic/MiniSat.hs
+++ Qlogic/MiniSat.hs
@@ -21,10 +21,10 @@ along with the Haskell Qlogic Library.  If not, see <http://www.gnu.org/licenses
 module Qlogic.MiniSat where
 
 import qualified Control.Monad.State.Lazy as State
-import Control.Concurrent (forkIO, killThread)
+import Control.Concurrent.Utils (spawn)
 import Control.Concurrent.MVar (takeMVar, putMVar, newEmptyMVar)
 import Control.Exception (evaluate, handle, AsyncException(..))
-import Control.Monad (when)
+import Control.Monad (liftM)
 import System.IO (hClose, hGetContents, hFlush, hPutStr)
 import qualified Data.IntSet as Set
 import qualified Data.List as List
@@ -49,21 +49,12 @@ type MiniSat r = SatSolver MiniSatSolver MiniSatLiteral r
 instance Solver MiniSatSolver MiniSatLiteral where
     solve                 = do mv <- liftIO newEmptyMVar
                                st <- State.get
-                               liftIO $ forkIO $ minithread mv $ addedFormula st
-                               satresult <- liftIO $ takeMVar mv
-                               case satresult of
--- handle (\ThreadKilled -> putMVar mv Nothing)
-                                 Just satassign -> do (mapM_ addposass $ filter ((<) 0) $ map (read :: String -> Int) $ words satassign)
-                                                      -- st'' <- State.get
-                                                      -- liftIO $ putStrLn $ show $ assign st''
-                                                      return True
-                                                        where addposass l = do st' <- State.get
-                                                                               State.put st'{assign = Set.insert l $ assign st'}
-                                 Nothing        -> return False
-                                 where minithread mv cnf = do (exitcode1, stdout, stderr) <- readProcessWithExitCode "minisat" ["/dev/stdin", "/dev/stdout"] cnf
-                                                              case lines stdout of
-                                                                "SAT" : satassign : _ -> putMVar mv $ Just satassign
-                                                                _                     -> putMVar mv Nothing
+                               out <- liftIO $ spawn "minisat" [] $ addedFormula st
+                               case lines `liftM` out of     
+                                 Just ("SAT" : satassign : _) -> mapM_ add poslits >> return True
+                                     where poslits = filter ((<) 0) $ [read l | l <- words satassign]
+                                           add l   = State.modify (\ st -> st{assign = Set.insert l $ assign st})
+                                 Nothing                      -> return False
     run m                 = State.evalStateT m emptySt
     newLit                = do st <- State.get
                                State.put st{lastLit = lastLit st + 1}
@@ -75,43 +66,3 @@ instance Solver MiniSatSolver MiniSatLiteral where
     getModelValue l       = do st <- State.get
                                return $ Set.member l $ assign st
 
--- myReadProcessWithExitCode
---     :: FilePath                 -- ^ command to run
---     -> [String]                 -- ^ any arguments
---     -> String                   -- ^ standard input
---     -> IO (ExitCode,String,String) -- ^ exitcode, stdout, stderr
--- myReadProcessWithExitCode cmd args input = do
---     (Just inh, Just outh, Just errh, pid) <-
---         createProcess (proc cmd args){ std_in  = CreatePipe,
---                                        std_out = CreatePipe,
---                                        std_err = CreatePipe }
--- 
---     outMVar <- newEmptyMVar
--- 
---     -- fork off a thread to start consuming stdout
---     out <- hGetContents outh
---     outtid <- forkIO $ evaluate (length out) >> putMVar outMVar ()
--- 
---     -- fork off a thread to start consuming stderr
---     err <- hGetContents errh
---     errtid <- forkIO $ evaluate (length err) >> putMVar outMVar ()
--- 
---     -- now write and flush any input
---     when (not (null input)) $ do hPutStr inh input; hFlush inh
---     hClose inh -- done with stdin
--- 
---     handle (\ThreadKilled -> do hClose outh
---                                 hClose errh
---                                 killThread outtid
---                                 killThread errtid
---                                 terminateProcess pid
---                                 return (ExitFailure 1, out, err))
---       (do takeMVar outMVar
---           takeMVar outMVar
---           hClose outh
---           hClose errh
--- 
---           -- wait on the process
---           ex <- waitForProcess pid
--- 
---           return (ex, out, err))
